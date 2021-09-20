2689b061e941458df7a67607659949dd19a6ac17
vipul devnani <vipuldevnani97@gmail.com>
2021-09-21 00:32:23 +0530

    Created controller for adding products to DB

---
+++ controllers/product.js
@@ -0,0 +1,53 @@
+const Product = require('../models/product');
+const _ = require('lodash');
+const formidable = require('formidable');
+const fs = require('fs');
+
+exports.getProductById = (req,res,next,id) => {
+    Product.findById(id)
+        .populate('category')
+        .exec((err,product) => {
+            if(err || !product){
+                res.status(400).json({
+                    error:"Could not find product"
+                });
+            }
+            req.product = product;
+            next();
+        });    
+}
+
+
+exports.createProduct = (req,res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req,(err,fields,file) => {
+        if(err){
+            return res.status(400).json({
+                error:"problem with image"
+            });
+        }
+        //TODO:restrictions on fields
+        let product = new Product(fields);
+
+        //handle file here
+        if(file.photo){
+            if(file.photo.size>3000000){
+                return res.status(400).json({
+                    error:"File size too big!"
+                });
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+        //save to db
+        product.save((err,savedProduct) => {
+            if(err){
+                res.status(400).json({
+                    error:"Saving tshirt in DB failed"
+                });
+            }
+            res.status(200).json(savedProduct);
+        });
+    });
+}
---
+++ routes/product.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const {getProductById,createProduct} = require('../controllers/product');
+const {getUserById, getUser} = require('../controllers/user');
+const {isAdmin,isAuthenticated,isSignedIn} = require('../controllers/auth');
+
+//all of params
+router.param('userId',getUserById);
+router.param('productId',getProductById);
+
+//all of actual routes
+router.post('/product/create/:userId',isSignedIn,isAuthenticated,isAdmin,createProduct);
+
+module.exports = router;
